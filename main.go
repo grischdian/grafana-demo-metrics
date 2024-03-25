da0c8f8fc1eb2df5d671bb4206920362ec521717
Christian Rost <dev@grischdian.de>
2024-03-25 21:19:06 +0100

    update metric

--- main.go
+++ main.go
@@ -37,64 +37,64 @@ func metricsHandler(w http.ResponseWriter, r *http.Request) {
     metrics := fmt.Sprintf(`
 # HELP cpu_usage CPU usage percentage
 # TYPE cpu_usage gauge
-cpu_usage{service="myService"} %f
+b1_cpu_usage{service="myService"} %f
 # HELP ram_usage RAM usage percentage
 # TYPE ram_usage gauge
-ram_usage{service="myService"} %f
+b1_ram_usage{service="myService"} %f
 # HELP disk_usage Disk usage percentage
 # TYPE disk_usage gauge
-disk_usage{service="myService"} %f
+b1_disk_usage{service="myService"} %f
 # HELP network_in Incoming network traffic in Mbps
 # TYPE network_in gauge
-network_in{service="myService"} %f
+b1_network_in{service="myService"} %f
 # HELP network_out Outgoing network traffic in Mbps
 # TYPE network_out gauge
-network_out{service="myService"} %f
+b1_network_out{service="myService"} %f
 # HELP error_rate Error rate percentage
 # TYPE error_rate gauge
-error_rate{service="myService"} %f
+b1_error_rate{service="myService"} %f
 # HELP response_time Average response time in ms
 # TYPE response_time gauge
-response_time{service="myService"} %f
+b1_response_time{service="myService"} %f
 # HELP db_connection_count Number of database connections
 # TYPE db_connection_count gauge
-db_connection_count{service="myService"} %d
+b1_db_connection_count{service="myService"} %d
 # HELP cache_hit_rate Cache hit rate percentage
 # TYPE cache_hit_rate gauge
-cache_hit_rate{service="myService"} %f
+b1_cache_hit_rate{service="myService"} %f
 # HELP goroutine_count Number of goroutines
 # TYPE goroutine_count gauge
-goroutine_count{service="myService"} %d
+b1_goroutine_count{service="myService"} %d
 # HELP thread_count Number of threads
 # TYPE thread_count gauge
-thread_count{service="myService"} %d
+b1_thread_count{service="myService"} %d
 # HELP request_count Number of requests
 # TYPE request_count counter
-request_count{service="myService"} %d
+b1_request_count{service="myService"} %d
 # HELP successful_requests Number of successful requests
 # TYPE successful_requests counter
-successful_requests{service="myService"} %d
+b1_successful_requests{service="myService"} %d
 # HELP failed_requests Number of failed requests
 # TYPE failed_requests counter
-failed_requests{service="myService"} %d
+b1_failed_requests{service="myService"} %d
 # HELP heap_alloc Bytes allocated and still in use
 # TYPE heap_alloc gauge
-heap_alloc{service="myService"} %d
+b1_heap_alloc{service="myService"} %d
 # HELP mallocs Total number of mallocs
 # TYPE mallocs counter
-mallocs{service="myService"} %d
+b1_mallocs{service="myService"} %d
 # HELP frees Total number of frees
 # TYPE frees counter
-frees{service="myService"} %d
+b1_frees{service="myService"} %d
 # HELP live_objects Number of live objects (mallocs - frees)
 # TYPE live_objects gauge
-live_objects{service="myService"} %d
+b1_live_objects{service="myService"} %d
 # HELP gc_pause Total GC pause time in ms
 # TYPE gc_pause gauge
-gc_pause{service="myService"} %f
+b1_gc_pause{service="myService"} %f
 # HELP last_gc Time of last garbage collection in Unix timestamp
 # TYPE last_gc gauge
-last_gc{service="myService"} %d
+b1_last_gc{service="myService"} %d
 `, cpuUsage, ramUsage, diskUsage, networkIn, networkOut, errorRate, responseTime, dbConnectionCount,
         cacheHitRate, goroutineCount, threadCount, requestCount, successfulRequests, failedRequests,
         heapAlloc, mallocs, frees, liveObjects, gcPause, lastGC)
